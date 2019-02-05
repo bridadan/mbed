65d4f2ceedcb9328f3f52222375cf48521015fb3
Brian Daniels <brianddaniels@gmail.com>
2019-02-05 11:00:39 -0600

    Fix building and converting of hex files

--- tools/regions.py
+++ tools/regions.py
@@ -20,8 +20,10 @@ LIBRARIES BUILD
 from __future__ import print_function, division, absolute_import
 
 import sys
-from os.path import join, abspath, dirname
+from os.path import join, abspath, dirname, split, splitext, normpath
 import json
+from shutil import copyfile
+from argparse import ArgumentParser
 
 
 # Be sure that the tools directory is in the search path
@@ -31,13 +33,41 @@ sys.path.insert(0, ROOT)
 from tools.build_api import merge_region_list
 from tools.notifier.term import TerminalNotifier
 from tools.config import Region
+from tools.utils import split_path, run_cmd_ext
+
+
 if __name__ == '__main__':
+    parser = ArgumentParser()
+
+    parser.add_argument('-k', '--toolchain_path', help='Path to toolchain')
+
+    parser.add_argument('-o', '--linker_output', help='Path to the built elf file')
+
+    options = parser.parse_args()
+    linker_output_directory, linker_output_name, linker_output_ext = split_path(options.linker_output)
+
+    hex_file = "BUILD/mbed-cloud-client-example.hex"
+    axf_file = normpath(join(linker_output_directory, linker_output_name + ".axf"))
+    copyfile(options.linker_output, axf_file)
+    command = [
+        join(normpath(options.toolchain_path), "ARM/ARMCC/bin/fromelf.exe"),
+        "--i32", "--output", normpath(options.linker_output), axf_file
+    ]
+    stdout, stderr, retcode = run_cmd_ext(command)
+
+    if retcode:
+        raise Exception(
+            "Failed to convert axf to hex.\r\ncommand: {}\r\nstdout: {}\r\nstderr: {}".format(
+                command, stdout, stderr
+            )
+        )
+
     with open(join("region_list.json"), "r") as region_list_file:
         region_list_data = json.load(region_list_file)
     region_list = [Region(*r) for r in region_list_data]
     merge_region_list(
         region_list,
-        "BUILD/mbed-cloud-client-example.hex",
+        hex_file,
         TerminalNotifier()
     )
 
